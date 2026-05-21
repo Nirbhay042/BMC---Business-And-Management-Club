d80f36c3e69c37540bf59b1fae7364b973b7240b
Prashant Prabhakar <prashant273000@gmail.com>
2026-05-21 15:32:21 +0530

    fix vercel css issue

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-
-  base: '/BMC---Buisness-And-Management-Club/',
-
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
@@ -20,5 +17,4 @@ export default defineConfig({
     open: true,
     port: 3000
   }
-
 });
