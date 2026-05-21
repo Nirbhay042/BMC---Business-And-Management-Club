b595a321cbb7c5099acf50420301c1f7ed972474
Prashant Prabhakar <83294769+prashant273000@users.noreply.github.com>
2026-05-21 14:46:07 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,23 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+
+  base: '/BMC---Buisness-And-Management-Club/',
+
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
     }
   },
+
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
   },
+
   server: {
     open: true,
     port: 3000
   }
+
 });
